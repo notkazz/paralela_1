c7441d6b7bb9b9b400a0f692f637419d5ff055af
Thales Santin <kazthales@gmail.com>
2024-09-18 00:58:33 -0300

    updates para uma unica imagem

--- app.c
+++ app.c
@@ -14,42 +14,39 @@ int main() {
         return -1;
     }
 
-    // Processamento das imagens
+    // Processamento da imagem
     double starttime = omp_get_wtime();
 
-    #pragma omp parallel for schedule(dynamic)
-    for (int i = 1; i <= 1; i++) {
-        // Constrói o caminho para a imagem
-        std::string path = "/app/Imagens/" + std::to_string(i) + ".PNG";
-        cv::Mat img = cv::imread(path);
+    // Carrega uma imagem
+    std::string path = "/app/Imagens/1.PNG";
+    cv::Mat img = cv::imread(path);
 
-        if (img.empty()) {
-            printf("Erro ao carregar a imagem %s\n", path.c_str());
-            continue;
-        }
-
-        // Conversão para escala de cinza
-        cv::Mat gray;
-        cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-        cv::equalizeHist(gray, gray);
-
-        // Vetor para armazenar as faces detectadas
-        std::vector<cv::Rect> faces;
-
-        // Detecção de faces
-        face_cascade.detectMultiScale(gray, faces);
-
-        // Desenha retângulos ao redor das faces detectadas
-        for (size_t j = 0; j < faces.size(); j++) {
-            cv::rectangle(img, faces[j], cv::Scalar(255, 0, 0), 2);
-        }
-
-        // Salva a imagem processada com faces detectadas
-        std::string output_path = "/app/Imagens/output_" + std::to_string(i) + ".PNG";
-        cv::imwrite(output_path, img);
-
-        printf("Imagem %s processada e salva em %s\n", path.c_str(), output_path.c_str());
+    if (img.empty()) {
+        printf("Erro ao carregar a imagem %s\n", path.c_str());
+        return -1;
     }
+
+    // Conversão para escala de cinza
+    cv::Mat gray;
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    cv::equalizeHist(gray, gray);
+
+    // Vetor para armazenar as faces detectadas
+    std::vector<cv::Rect> faces;
+
+    // Detecção de faces
+    face_cascade.detectMultiScale(gray, faces);
+
+    // Desenha retângulos ao redor das faces detectadas
+    for (size_t j = 0; j < faces.size(); j++) {
+        cv::rectangle(img, faces[j], cv::Scalar(255, 0, 0), 2);
+    }
+
+    // Salva a imagem processada com faces detectadas
+    std::string output_path = "/app/Imagens/output_1.PNG";
+    cv::imwrite(output_path, img);
+
+    printf("Imagem %s processada e salva em %s\n", path.c_str(), output_path.c_str());
 
     double stoptime = omp_get_wtime();
     printf("Tempo de execução paralelo: %3.2f segundos\n", stoptime - starttime);
